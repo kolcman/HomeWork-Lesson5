a62c007b83057b9f5043d53b63b7e47ba439b6eb
Kolcman <m.kolcman@yandex.ru>
2024-02-16 17:47:45 +0300

    Lesson:Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,19 @@
+﻿// Task3
+
+// Быстро.
+Array arrayOfNumbers = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+Array.Reverse(arrayOfNumbers);
+foreach(int e in arrayOfNumbers)
+{
+    System.Console.Write($"{e} ");
+}
+
+// Не Быстро
+
+// for (int i = 0; i < arrayOfNumbers.Length / 2; i++)
+// {
+// temp = arrayOfNumbers[i];
+// arrayOfNumbers[i] = arrayOfNumbers[arrayOfNumbers.Length - 1 - i];
+// arrayOfNumbers[arrayOfNumbers.Length - 1 - i] = temp;
+// }
+
