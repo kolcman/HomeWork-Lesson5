1c64ff2dda8a42235879599c4150307c95097e4a
Kolcman <m.kolcman@yandex.ru>
2024-02-16 17:31:23 +0300

    Lesson:Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,22 @@
+﻿// Task 2
+
+void Main()
+{
+    
+    int[] arrOfNumb = new int[10];
+    for (int i = 0; i < arrOfNumb.Length; i++)
+    {
+        arrOfNumb[i] = new Random().Next(100, 1000);
+    }
+    int count = 0;
+    foreach (int e in arrOfNumb)
+    {
+        if (e % 2 == 0)
+        {
+            count++;
+        }
+    }
+    System.Console.WriteLine(count);
+}
+
+Main();
