a3e96a5ca7350bc0e022a92497ce00686c54f8eb
Kolcman <m.kolcman@yandex.ru>
2024-02-16 16:36:42 +0300

    Lesson:Task1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Task1
+void Main()
+{
+    searchQuit();
+
+}
+
+string readNumber(string msg)
+{
+    System.Console.WriteLine(msg);
+    return Console.ReadLine();
+    
+}
+
+void searchQuit()
+{
+    while (true)
+    {
+        string input = readNumber("Введите число: ");
+        if(input == "q")
+        {
+            break;
+        }
+        
+        if (int.TryParse(input, out int number))
+        {
+            if ((number > 0) && ((number / 10 + number % 10) % 2 == 0))
+            {
+                break;
+            }
+        }
+    }
+}
+
+
+Main();
